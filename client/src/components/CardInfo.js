483767ecb36298acfee06409d54bb1f6daef600b
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2022-10-09 01:11:24 +0530

    card added

---
+++ client/src/components/CardInfo.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+const CardInfo = () => {
+    const [cardInfo, setCardInfo] = useState(5)
+
+  return (
+      <div>
+          
+          
+          
+    </div>
+  )
+}
+
+export default CardInfo
---
+++ client/src/components/Cards.js
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Cards() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+
+      <CardContent>
+        <Typography paragraph>
+          Heat 1/2 cup of the broth in a pot until simmering, add saffron and
+          set aside for 10 minutes.
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+}
